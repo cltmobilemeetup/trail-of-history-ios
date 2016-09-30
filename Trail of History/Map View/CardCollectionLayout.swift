9e8462dcae8f166028054d2a40e4cdce7da9e692
Robert Vaessen <robert@rvaessen.com>
2016-09-30 07:25:55 -0400

    Minor tweaks for clarity.

--- Trail of History/Map View/CardCollectionLayout.swift
+++ Trail of History/Map View/CardCollectionLayout.swift
@@ -14,8 +14,8 @@ class CardCollectionLayout: UICollectionViewFlowLayout {
 
         // The user has stopped scrolling the collection view. The proposed content offset tells us where the left hand side of the
         // collection view (i.e. the visible portion of the collection) will be positioned relative to the start of the collection's
-        // content (the total width of the content is greater than the width of the collection view). We are being given the opportunity
-        // to alter that offset.
+        // content (the total width of the content is greater than the width of the view). We are being given the opportunity to
+        // alter that offset.
         //
         // We are going to alter the offset such that the collection view will be centered on a cell. The cell that we will choose
         // is the one whose center would be closest to the center of the collection view were we to accept the proposed offset.
@@ -25,12 +25,12 @@ class CardCollectionLayout: UICollectionViewFlowLayout {
             let collectionViewBounds = collectionView.bounds
             let halfWidth = collectionViewBounds.size.width * 0.5;
 
-            // This is the content offset that would occupy the center of the collection view were we to accept the proposed offset.
-            let proposedCenterX = proposedContentOffset.x + halfWidth;
+            // This is the content offset at which the collection view would be centered were we to accept the proposed (lefthand side) offset.
+            let proposedCenterOffset = proposedContentOffset.x + halfWidth;
             
             if let attributesForVisibleCells = self.layoutAttributesForElementsInRect(collectionViewBounds) {
 
-                // Find the cell that would be closest to the center of the collection view were we to accept the proposed offset.
+                // Find the cell whose center would be closest to the center of the collection view were we to accept the proposed offset.
                 var target: UICollectionViewLayoutAttributes?
                 for candidate in attributesForVisibleCells where candidate.representedElementCategory == UICollectionElementCategory.Cell {
                     
@@ -38,8 +38,8 @@ class CardCollectionLayout: UICollectionViewFlowLayout {
                         target = candidate;
                     }
                     else {
-                        let targetDistance = target!.center.x - proposedCenterX
-                        let candidateDistance = candidate.center.x - proposedCenterX
+                        let targetDistance = target!.center.x - proposedCenterOffset
+                        let candidateDistance = candidate.center.x - proposedCenterOffset
 
                         if fabsf(Float(candidateDistance)) < fabsf(Float(targetDistance)) {
                             target = candidate;
@@ -47,7 +47,7 @@ class CardCollectionLayout: UICollectionViewFlowLayout {
                     }
                 }
  
-                // Adjust the offset such that our target cell will be centered in the collection view.
+                // Adjust the (lefthand side) offset such that our target cell will be centered in the collection view.
                 return CGPoint(x: round(target!.center.x - halfWidth), y: proposedContentOffset.y)
             }
         }
