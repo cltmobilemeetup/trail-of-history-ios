26524b433204c64d9cf536046b88222ed647e95c
markflowers <mark.s.flowers@gmail.com>
2016-10-18 19:24:37 -0400

    Fixing things that were mysteriously broken by Swift 3 upgrade.

--- Trail of History/List View/ListViewController.swift
+++ Trail of History/List View/ListViewController.swift
@@ -10,20 +10,17 @@ import UIKit
 
 class ListViewController: UICollectionViewController {
 
-		private let identifierPOICell = String(POICollectionViewCell)
-		private lazy var collectionViewWidth: CGFloat = {
-			return CGRectGetWidth(self.collectionView!.bounds)
-		}()
-	
-		private let poiCellHeight = CGFloat(100.0)
+    private let identifierPOICell = String(POICollectionViewCell)
+    private lazy var collectionViewWidth: CGFloat = {
+        return self.collectionView!.bounds.width
+    }()
+
+    private let poiCellHeight = CGFloat(100.0)
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< Updated upstream
 				collectionView?.registerNib(UINib(nibName: identifierPOICell, bundle: nil), forCellWithReuseIdentifier: identifierPOICell)
 				collectionView?.backgroundColor = UIColor.tohGreyishBrownColor()
-=======
->>>>>>> Stashed changes
     }
 	
 }
@@ -32,19 +29,18 @@ class ListViewController: UICollectionViewController {
 
 extension ListViewController {
 	
-	override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
 		let cell = collectionView.dequeueReusableCellWithReuseIdentifier("POICollectionViewCell", forIndexPath: indexPath) as! POICollectionViewCell
 		return cell
 	}
 	
-	override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 		return 10
 	}
 	
-	override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+	func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
 		return 1
 	}
-	
 	
 	func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
 		return CGSize(width: collectionViewWidth, height: poiCellHeight)
@@ -60,22 +56,12 @@ extension ListViewController {
 	
 }
 
-<<<<<<< Updated upstream
 // MARK: UICollectionViewDelegate
 
 extension ListViewController {
 	
 	override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-		
-	}
-	
-	
-=======
-    // MARK: UICollectionViewDelegate
-    
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-    }
+
     
     // MARK: UICollectionViewDataSource
     
@@ -91,6 +77,5 @@ extension ListViewController {
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
->>>>>>> Stashed changes
 }
 
