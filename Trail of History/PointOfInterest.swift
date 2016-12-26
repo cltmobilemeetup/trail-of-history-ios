00226d3fde3881e2807015663bfc59907f2bae0f
Robert Vaessen <robert@rvaessen.com>
2016-12-26 13:11:43 -0500

    Created a PointOfInterest notifier that notifies listeners of each of the Firebase's existing point of interest records and notifies them of any additions, updates or removals. Modified the Map View such that it is dynamically driven by the notifier (previously it had been coded with the assumption that the list of points of interest was static, i.e. completely known at startup time). Thus the Map View responds in real time to any data changes that are made via the Firebase console.

--- Trail of History/PointOfInterest.swift
+++ Trail of History/PointOfInterest.swift
@@ -2,200 +2,217 @@
 //  PointOfInterest.swift
 //  Trail of History
 //
-//  Created by Robert Vaessen on 12/18/16.
+//  Created by Robert Vaessen on 12/23/16.
 //  Copyright © 2016 CLT Mobile. All rights reserved.
 //
 
-import CoreLocation
 import UIKit
 import Firebase
+import CoreLocation
 
 class PointOfInterest {
-    private static var pointsOfInterest: [PointOfInterest]!
-    private static var loadingCompleted = false
-    private static var callbacks = [(PointOfInterest) -> Void]()
-    private static let queue = DispatchQueue(label: "Points of Interest")
 
-    static func getAll(callback: @escaping (PointOfInterest) -> Void) {
-        queue.async {
-            if pointsOfInterest == nil {
-                pointsOfInterest = [PointOfInterest]()
-                //loadFromFile()
-                loadFromFirebase()
+    class Notifier {
+
+        enum Event {
+            case added
+            case updated
+            case removed
+        }
+        
+        typealias Listener = (PointOfInterest, Event) -> Void
+        
+        class Token {
+            fileprivate init() {}
+        }
+        
+        private class Registrant : Token {
+            
+            private var listener: Listener?
+            private var dispatchQueue: DispatchQueue?
+            private var reference: FIRDatabaseReference?
+            
+            fileprivate init(listener: @escaping Listener, dispatchQueue: DispatchQueue) {
+                self.listener = listener
+                self.dispatchQueue = dispatchQueue
+                self.reference = FIRDatabase.database().reference(withPath: "pointOfInterest")
+                
+                super.init()
+                
+                reference!.observe(.childAdded,   with: { self.notify(properties: $0.value as! [String: Any], event: .added) })
+                reference!.observe(.childChanged, with: { self.notify(properties: $0.value as! [String: Any], event: .updated) })
+                reference!.observe(.childRemoved, with: { self.notify(properties: $0.value as! [String: Any], event: .removed) })
             }
             
-            // Execute the callback for all points of interest that have already been loaded
-            for poi in pointsOfInterest {
-                DispatchQueue.main.async {
-                    callback(poi)
+            deinit {
+                cancel()
+            }
+            
+            fileprivate func cancel() {
+                if let ref = reference {
+                    ref.removeAllObservers()
+                    reference = nil
+                    
+                    listener = nil
+                    dispatchQueue = nil
                 }
             }
-            // If we are still waiting for some points of interest to arrive
-            // then save the callback so that it can be executed when they do.
-            if !loadingCompleted {
-                callbacks.append(callback)
+            
+            private func notify(properties: [String: Any], event: Event) {
+                if let poi = PointOfInterest(properties: properties) {
+                    loadImage(poi: poi, event: event)
+                }
+                else {
+                    print("Invalid POI data: \(properties)")
+                }
             }
-        }
-    }
-    
-    static private func loadFromFirebase() {
-        
-        let reference = FIRDatabase.database().reference(withPath: "pointOfInterest")
-        reference.observe(.value, with: { parent in
-            if parent.childrenCount > 0 {
-                for child in parent.children {
-                    let value = (child as! FIRDataSnapshot).value
-                    let properties = value as! [String: Any]
-                    if let poi = PointOfInterest(properties: properties) {
-                        loadImage(poi: poi, totalPoiCount: Int(parent.childrenCount))
+            
+            private func loadImage(poi: PointOfInterest, event: Event) {
+                let session = URLSession(configuration: .default)
+                let imageDownloadTask = session.dataTask(with: poi.imageUrl) { (data, response, error) in
+                    
+                    var image: UIImage?
+                    var errorText = ""
+                    
+                    if let error = error {
+                        errorText = "Error = \(error)"
                     }
                     else {
-                        print("Invalid POI data: \(properties)")
-                    }
-                }
-            }
-            else {
-                loadingCompleted = true
-            }
-        })
-    }
-    
-    static private func loadFromFile() {
-        
-        if  let filePath = Bundle.main.path(forResource: "PointsOfInterest", ofType: "json"),
-            let rawData = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
-            let jsonData = try? JSONSerialization.jsonObject(with: rawData) {
-            
-            let container = jsonData as! [String : Dictionary<String, Dictionary<String, Any>>]
-            let list = container["pointOfInterest"]!
-            if list.count > 0 {
-                for (_, properties) in list {
-                    if let poi = PointOfInterest(properties: properties) {
-                        loadImage(poi: poi, totalPoiCount: list.count)
-                    }
-                    else {
-                        print("Invalid POI data: \(properties)")
-                    }
-                }
-            }
-            else {
-                loadingCompleted = true
-            }
-        }
-        else {
-            loadingCompleted = true
-        }
-    }
-
-    static private func loadImage(poi: PointOfInterest, totalPoiCount: Int) {
-        let session = URLSession(configuration: .default)
-        let imageDownloadTask = session.dataTask(with: poi.imageUrl) { (data, response, error) in
-
-            var image: UIImage?
-            var errorText = ""
-            
-            if let error = error {
-                errorText = "Error = \(error)"
-            }
-            else {
-                if let response = response as? HTTPURLResponse {
-                    if let data = data {
-                        image = UIImage(data: data)
-                        if image == nil {
-                            errorText = "UIImage could not be created (image data is corrupt?)"
+                        if let response = response as? HTTPURLResponse {
+                            if let data = data {
+                                image = UIImage(data: data)
+                                if image == nil {
+                                    errorText = "UIImage could not be created (image data is corrupt?)"
+                                }
+                            }
+                            else {
+                                errorText = "Image data = nil (http response code = \(response.statusCode).)"
+                            }
+                        }
+                        else {
+                            errorText = "Http response = nil"
                         }
                     }
-                    else {
-                        errorText = "Image data = nil (http response code = \(response.statusCode).)"
+                    
+                    // If the image could not be obtained then create a "standin" image that will inform the user.
+                    if image == nil {
+                        image = self.generateImage(from: "\(poi.name)'s image could not be downloaded\n\n[image url: \(poi.imageUrl)]\n[details: \(errorText)]")
+                    }
+                    
+                    poi.image = image!
+                    
+                    self.dispatchQueue!.async {
+                        self.listener!(poi, event)
+                    }
+                    
+                }
+                imageDownloadTask.resume()
+            }
+            
+            private func generateImage(from: String) -> UIImage {
+                let image = UIImage(named: "blank")!
+                
+                let imageView = UIImageView(image: image)
+                imageView.backgroundColor = UIColor.clear
+                imageView.frame = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
+                
+                let label = UILabel(frame: imageView.frame)
+                label.backgroundColor = UIColor.clear
+                label.textAlignment = .center
+                label.textColor = UIColor.white
+                label.text = from
+                
+                UIGraphicsBeginImageContextWithOptions(label.bounds.size, false, 0);
+                imageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+                label.layer.render(in: UIGraphicsGetCurrentContext()!)
+                let textImage = UIGraphicsGetImageFromCurrentImageContext()!
+                UIGraphicsEndImageContext();
+                
+                return textImage
+            }
+        }
+        
+        fileprivate init() {}
+        
+        func register(listener: @escaping Listener, dispatchQueue: DispatchQueue) -> Token {
+            return Registrant(listener: listener, dispatchQueue: dispatchQueue)
+        }
+        
+        func deregister(token: Token) {
+            (token as? Registrant)?.cancel()
+        }
+        
+        private func loadFrom(file: String) -> [PointOfInterest]? {
+            
+            var pointsOfInterest: [PointOfInterest]?
+            
+            let components = file.components(separatedBy: ".")
+            if  components.count == 2,
+                let filePath = Bundle.main.path(forResource: components[0], ofType: components[1]),
+                let rawData = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
+                let jsonData = try? JSONSerialization.jsonObject(with: rawData) {
+                
+                let container = jsonData as! [String : Dictionary<String, Dictionary<String, Any>>]
+                let list = container["pointOfInterest"]!
+                if list.count > 0 {
+                    
+                    for (_, properties) in list {
+                        if let poi = PointOfInterest(properties: properties) {
+                            if pointsOfInterest == nil { pointsOfInterest = [PointOfInterest]() }
+                            pointsOfInterest!.append(poi)
+                        }
+                        else {
+                            print("Invalid POI data: \(properties)")
+                        }
                     }
                 }
                 else {
-                    errorText = "Http response = nil"
+                    print("The Points of Interest file does not contain any valid data")
                 }
             }
-            
-            // If the image could not be obtained then create a "standin" image that will inform the user.
-            if image == nil {
-                image = generateImage(from: "\(poi.name)'s image could not be downloaded\n\n[image url: \(poi.imageUrl)]\n[details: \(errorText)]")
-            }
-
-            poi.image = image!
-
-            finishLoading(poi: poi, totalPoiCount: totalPoiCount)
-        }
-        imageDownloadTask.resume()
-    }
-
-    static private func finishLoading(poi: PointOfInterest, totalPoiCount: Int) {
-        
-        queue.async {
-            pointsOfInterest.append(poi)
-            loadingCompleted = pointsOfInterest.count == totalPoiCount
-            
-            for callback in callbacks {
-                DispatchQueue.main.async {
-                    callback(poi)
-                }
+            else {
+                print("The Points of Interest file cannot be loaded/parsed")
             }
             
-            if loadingCompleted {
-                callbacks.removeAll()
-            }
+            return pointsOfInterest
         }
     }
 
-    static func sort() -> [PointOfInterest] {
-        return pointsOfInterest.sorted { $0.coordinate.longitude < $1.coordinate.longitude }
-
-    }
+    static let notifier = Notifier()
     
-    private static func generateImage(from: String) -> UIImage
-    {
-        let image = UIImage(named: "blank")!
-        
-        let imageView = UIImageView(image: image)
-        imageView.backgroundColor = UIColor.clear
-        imageView.frame = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
-        
-        let label = UILabel(frame: imageView.frame)
-        label.backgroundColor = UIColor.clear
-        label.textAlignment = .center
-        label.textColor = UIColor.white
-        label.text = from
-        
-        UIGraphicsBeginImageContextWithOptions(label.bounds.size, false, 0);
-        imageView.layer.render(in: UIGraphicsGetCurrentContext()!)
-        label.layer.render(in: UIGraphicsGetCurrentContext()!)
-        let textImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext();
-        
-        return textImage
-    }
-
-    //**************************************************************************************************************
-    
+    let id: String
     let name: String
     let description: String
     let coordinate: CLLocationCoordinate2D
 
-    private let imageUrl: URL
-    private(set) var image: UIImage!
+    fileprivate(set) var image: UIImage!
+    fileprivate let imageUrl: URL
 
-    private init?(properties: Dictionary<String, Any>) {
-        if  let name = properties["name"], let latitude = properties["latitude"], let longitude = properties["longitude"], let description = properties["description"], let imageUrl = properties["imageUrl"] {
+    private let location: CLLocation
 
+    fileprivate init?(properties: Dictionary<String, Any>) {
+        if  let id = properties["uid"], let name = properties["name"], let latitude = properties["latitude"],
+            let longitude = properties["longitude"], let description = properties["description"], let imageUrl = properties["imageUrl"] {
+            
+            self.id = id as! String
             self.name = name as! String
             self.description = description as! String
-
+            
             self.coordinate = CLLocationCoordinate2D(latitude: latitude as! Double, longitude: longitude as! Double)
-
+            location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            
             let url = URL(string: imageUrl as! String)
             if url == nil { return nil }
             self.imageUrl = url!
-
+            
         }
         else {
             return nil
         }
+    }
+
+    func distance(to location: CLLocation) -> Double {
+        let YardsPerMeter = 1.0936
+        return location.distance(from: self.location) * YardsPerMeter
     }
 }
